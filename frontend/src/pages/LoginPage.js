4cc30d26726492337e95c77d4e14639c2b8c1eca
ddelacruz42 <91350250+ddelacruz42@users.noreply.github.com>
2023-07-03 14:11:23 -0400

    Modified login page to switch between login and register

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -2,14 +2,45 @@ import React from 'react';
 
 import PageTitle from '../components/PageTitle';
 import Login from '../components/Login';
+import Register from '../components/Register';
+import ToggleSwitch from '../components/ToggleSwitch';
 
-const LoginPage = () =>
+function LoginPage()
 {
+    const [showLogin, setShowLogin] = useState(true);
+
+    const wantLogin = () => {
+      setShowLogin(true);
+      
+    };
+
+    const wantRegister = () => {
+      setShowLogin(false);
+      
+    };
 
     return(
-      <div>
-        <PageTitle />
-        <Login />
+      <div className="loginRegisterBox">
+            <div className="logo">
+              <PageTitle />
+            </div>
+  
+            <div className="loginRegisterToggle">
+              <ToggleSwitch
+                  isOn={showLogin}
+                  onColor="#339DFF"
+                  handleToggle={() => setShowLogin(!showLogin)}
+              />
+
+            </div>
+
+            <div className="loginRegisterView">
+              {showLogin ? (
+                <Login wantLogin={wantLogin} />
+              ) : (
+                <Register wantRegister={wantRegister} />
+              )}
+            </div>
       </div>
     );
 };
