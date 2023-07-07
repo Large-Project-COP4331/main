db4023dfa8daf0dd813c17cf1a7d4af21909f654
ddelacruz42 <91350250+ddelacruz42@users.noreply.github.com>
2023-07-07 11:48:30 -0400

    added visual feedback for inputs

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -24,6 +24,63 @@ function Register()
 
     const [message,setMessage] = useState('');
 
+    const [password, setPassword] = useState({
+        registerPassword: "",
+    });
+
+    const [username, setUsername] = useState({
+        registerUsername: "",
+    });
+
+    const [email, setEmail] = useState({
+        registerEmail: "",
+    });
+
+    const [showPasswordErr, setShowPasswordErr] = useState(true);
+    const [showUsernameErr, setShowUsernameErr] = useState(true);
+    const [showEmailErr, setShowEmailErr] = useState(true);
+
+    const [
+        userValidLength,
+        userHasNumber,
+        userUpperCase,
+        userlowerCase,
+    ] = useUsernameValidation({
+        registerUsername: username.registerUsername
+    });
+
+    const [
+        validLength,
+        hasNumber,
+        upperCase,
+        lowerCase,
+        specialChar
+    ] = usePasswordValidation({
+        registerPassword: password.registerPassword
+    });
+
+    const [
+        validEmail
+    ] = useEmailValidation({
+        registerEmail: email.registerEmail
+    });
+
+    const setRegisterUsername = (event) => 
+    {
+        setUsername({...username, registerUsername: event.target.value})
+    }
+
+
+    const setRegisterPassword = (event) => 
+    {
+        setPassword({...password, registerPassword: event.target.value})
+    }
+
+    const setRegisterEmail = (event) => 
+    {
+        setEmail({...email, registerEmail: event.target.value})
+    }
+
     // Function to call the register API.
     const doRegister = async event => 
     {
@@ -75,14 +132,35 @@ function Register()
                 <input type="text" className="registerField" id="registerLastName" placeholder="Last Name" 
                     ref={(c) => registerLastName = c} /><br />
 
-                <input type="text" className="registerField" id="registerUsername" placeholder="Username" 
-                    ref={(c) => registerUsername = c} /><br />
+                <input type="text" 
+                    className="registerField" 
+                    id="registerUsername" 
+                    placeholder="Username" 
+                    ref={(c) => registerUsername = c} 
+                    onChange={setRegisterUsername} 
+                    onFocus={()=>setShowUsernameErr(false)} 
+                    onBlur={()=>setShowUsernameErr(true)}
+                /><br />
 
-                <input type="password" className="registerField" id="registerPassword" placeholder="Password" 
-                    ref={(c) => registerPassword = c} /><br />
+                <input type="password" 
+                    className="registerField" 
+                    id="registerPassword" 
+                    placeholder="Password" 
+                    ref={(c) => registerPassword = c} 
+                    onChange={setRegisterPassword} 
+                    onFocus={()=>setShowPasswordErr(false)} 
+                    onBlur={()=>setShowPasswordErr(true)}
+                /><br />
 
-                <input type="text" className="registerField" id="registerEmail" placeholder="Email" 
-                    ref={(c) => registerEmail = c} /><br />
+                <input type="text" 
+                    className="registerField" 
+                    id="registerEmail" 
+                    placeholder="Email" 
+                    ref={(c) => registerEmail = c} 
+                    onChange={setRegisterEmail} 
+                    onFocus={()=>setShowEmailErr(false)} 
+                    onBlur={()=>setShowEmailErr(true)}
+                /><br />
 
                 <div className="registeDivBottom">
                     <input type="submit" id="registerButton" class="buttons" value = "Register"
@@ -90,6 +168,51 @@ function Register()
                 </div>
 
                 <span id="registerResult">{message}</span>
+
+                <div className={showUsernameErr? "usernameErrMsg" : "show"}  >
+                        <ul>
+                            <p id="usernameLength" className={userValidLength ? "valid" : "invalid"}>
+                                Username should be at least <b>8 characters</b>
+                            </p>
+                            <p id="usernameNumber" className={userHasNumber? "valid":"invalid"}>
+                                Username should have at least one <b>number</b>
+                            </p>
+                            <p id="usernameCapital" className={userUpperCase? "valid":"invalid"}>
+                                Username should have at least one <b>Uppercase letter</b>
+                            </p>
+                            <p id="usernameLowercase" className={userlowerCase? "valid":"invalid"}>
+                                Username should have at least one <b>Lowercase letter</b>
+                            </p>
+                        </ul>
+                </div>
+
+                <div className={showPasswordErr? "passwordErrorMsg" : "show"}  >
+                        <ul>
+                            <p id="passwordLength" className={validLength ? "valid" : "invalid"}>
+                                Password should be at least <b>8 characters</b>
+                            </p>
+                            <p id="passwordNumber" className={hasNumber? "valid":"invalid"}>
+                                Password should be at least <b>one number</b>
+                            </p>
+                            <p id="passwordCapital" className={upperCase? "valid":"invalid"}>
+                                Password should have at least one <b>Uppercase letter</b>
+                            </p>
+                            <p id="passwordLowercase" className={lowerCase? "valid":"invalid"}>
+                                Password should have at least one <b>Lowercase letter</b>
+                            </p>
+                            <p id="passwordSpecial" className={specialChar? "valid":"invalid"}>
+                                Password should have at least one <b>Special Character</b>
+                            </p>
+                        </ul>
+                </div>
+
+                <div className={showEmailErr? "emailErrMsg" : "show"}  >
+                        <ul>
+                            <p id="emailValid" className={validEmail ? "valid" : "invalid"}>
+                                Email should be: <b>name@mail.com</b>
+                            </p>
+                        </ul>
+                </div>
             </form>
         </div>
     );
