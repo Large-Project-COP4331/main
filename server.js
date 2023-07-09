fec3875dd9a15c15f4638c14ed5b5fa4ddd72060
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2023-07-08 20:48:05 -0400

    Tried to fix verification api in server.js

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ if (process.env.NODE_ENV === 'production')
   // Set static folder
   app.use(express.static('frontend/build'));
 
-  app.get('*', (req, res) => 
- {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
+ //  app.get('*', (req, res) => 
+ // {
+ //    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+ //  });
 }
 
 const path = require("path");
