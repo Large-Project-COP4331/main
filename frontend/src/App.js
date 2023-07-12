d3489c435b9f281c689ed1da1b28c0909f194db4
ddelacruz42 <91350250+ddelacruz42@users.noreply.github.com>
2023-07-12 12:14:52 -0700

    added more pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Welcome from './pages/Welcome';
 import LoginPage from './pages/LoginPage';
+import ForgotPage from './pages/ForgotPage';
+import RegisterPage from './pages/RegisterPage';
+import HomePage from './pages/HomePage';
 
 function App() 
 {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" index element={<LoginPage />} />
+      <Route path="/" index element={<Welcome />} />
+      <Route path="/loginpage" index element={<LoginPage />} />
+      <Route path="/forgotpage" index element={<ForgotPage />} />
+      <Route path="/registerpage" index element={<RegisterPage />} />
+      <Route path="/homepage" index element={<HomePage/>} />
     </Routes>
   </BrowserRouter>
 
