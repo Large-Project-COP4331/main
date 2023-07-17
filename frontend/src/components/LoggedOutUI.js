ba39bc50d2df9e0cc59075360e3e58fc589644a1
ddelacruz42 <91350250+ddelacruz42@users.noreply.github.com>
2023-07-17 06:37:11 -0700

    Update Visuals fixes typos

--- frontend/src/components/LoggedOutUI.js
+++ frontend/src/components/LoggedOutUI.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 
 import logo from './images/OceanLoggerLogo.png';
-import shark from './images/001-shark.png';
+import shark from './images/006-shark-1.png';
+import log from './images/003-time.png';
+import eye from './images/004-biometric-recognition.png';
+import shield from './images/005-shield.png';
+
 
 function LoggedOutUI()
 {
@@ -32,7 +36,7 @@ function LoggedOutUI()
                 </div>
             </div>
 
-            <div style={{height: 250}} />
+            <div style={{height: 150}} />
 
             <div className="toolsProvided">
                 <div className="bigText">
@@ -44,24 +48,63 @@ function LoggedOutUI()
                     </span>
                 </div>
                 <div className="scubbaTools" style={{display: 'flex', flexDirection: 'row'}}>
-                    <div className="tool1" />
-                    <div className="tool2" />
-                    <div className="tool3" />
-                </div>
-                
-                <div className="loggedOutBottom">
-                     {/**onClick={doRegister} */}
-                    <input type="submit" id="registerButton" class="buttons" value = "Sign Up Now"/>
+                    <div className="circle1">
+                        <div className="logIconCircle">
+                            <img id="logIcon" src={log}/>
+                        </div>
+                        <div className="logIconText" style={{fontWeight: 'bold'}}>Logs</div>
+                    </div>
+                    
+                    <div className="circle2">
+                        <div className="eyeIconCircle">
+                            <img id="eyeIcon" src={eye}/>
+                        </div>
+                        <div className="eyeIconText" style={{fontWeight: 'bold'}}>Capture</div>
+                    </div>
+                    
+                    <div className="circle3">
+                        <div className="shieldIconCircle">
+                            <img id="shieldIcon" src={shield}/>
+                        </div>
+                        <div className="shieldIconText" style={{fontWeight: 'bold'}}>Safety</div>
+                    </div>
                 </div>
             </div>
 
-            <div style={{height: 250}} />
+            <div className="loggedOutBottom">
+                <div className="loggedOutBottomRegister">
+                    <div className="loggedOutBottomRegisterText">
+                        <span style={{fontSize: 40, 
+                            fontWeight: '700', 
+                            wordWrap: 'break-word'}}
+                        >
+                            Want to Log<br/>
+                        </span>
+                        
+                        <span style={{fontSize: 40, 
+                            fontWeight: '700', 
+                            wordWrap: 'break-word'}}
+                        >
+                            Your Dive?<br/>
+                        </span>
+                        <span style={{fontSize: 12,  
+                            wordWrap: 'break-word'}}
+                        >
+                            Start logging your underwater experiences now!<br/>
+                        </span>
+                    </div>
+                    <div className="loggedOutBottomRegisterButton">
+                        {/**onClick={doRegister} or go to a link?*/}
+                        <input type="submit" id="registerButton" class="buttons" value = "Sign Up Now"/>
+                    </div>
+                </div>
+            </div>
 
             <div className="footer">
                 <div className="footerContent">
                     <div className="footerLogo">
                         <span id="blackText" style={{fontSize: 25}}>
-                            Scubba
+                            Scuba
                         </span>
                         <span id="blueText" style={{fontSize: 25}}>
                             Logger
@@ -76,7 +119,7 @@ function LoggedOutUI()
                 </div>
                
                <div style={{display: 'flex', justifyContent: 'center', marginTop: 10}}>
-                    © 2023 ScubbaLogger. All rights reserved.
+                    © 2023 ScubaLogger. All rights reserved.
                </div>
             </div> 
         </div>
