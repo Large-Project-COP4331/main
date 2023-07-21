b5f60d22fe53417a8757f3bd886911a09b218707
ddelacruz42 <91350250+ddelacruz42@users.noreply.github.com>
2023-07-21 14:43:40 -0700

    Update DiveCalcUI.js

--- frontend/src/components/DiveCalcUI.js
+++ frontend/src/components/DiveCalcUI.js
@@ -1,12 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEye} from "@fortawesome/free-solid-svg-icons";
-import { faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { useTotalTimeValidation } from '../hooks/useTotalTimeValidation';
-
-const eye = <FontAwesomeIcon icon={faEye}/>;
-const eyeSlash = <FontAwesomeIcon icon={faEyeSlash}/>;
 
 function DiveCalcUI()
 {
