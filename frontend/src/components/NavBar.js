0df16befeb2c0134a90a707b0889a95e863f4eec
ddelacruz42 <91350250+ddelacruz42@users.noreply.github.com>
2023-07-21 13:19:03 -0700

    redid navbar in react bootstrap

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,29 +1,39 @@
 import React from 'react';
-import {  Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import shark from './images/006-shark-1.png';
 
 const navbar= () =>{
   return (
-    <div>
-        <div class="left">
-            <div className="logo">
-                <span id="blackText" style={{fontSize: 25}}>
-                     Scuba
-                </span>
-                <span id="blueText" style={{fontSize: 25}}>
-                    Logger
-                </span>
-                <img src={shark}/>
-            </div>
-        </div>
-         
-        <div class="right">
-            <ul className="navBar">
-                <Link to="/loginpage" className="navLinks">Login</Link>
-                <Link to="/registerpage" className="navLinks">Register</Link>
-            </ul>
-        </div>
-    </div>
+      <Navbar expand="lg" className="bg-body-tertiary overflow-visible">
+        <Container>
+          <Navbar.Brand href="#welcome">
+              <span id="blackText" style={{fontSize: 25}}>
+                  Scuba
+              </span>
+              <span id="blueText" style={{fontSize: 25}}>
+                  Logger
+              </span>
+              <img
+                  alt="ScubbaLogger Logo"
+                  src={shark}
+                  width="30"
+                  height="30"
+              />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                  
+              </Nav>
+              <Nav>
+                  <Nav.Link href="/loginpage" >Login</Nav.Link>
+                  <Nav.Link href="/registerpage" >Register</Nav.Link>
+              </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
   );
 }
 export default navbar;
