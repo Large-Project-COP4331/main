7123a4a8e1fb5244c5781a932b6bdd788176db83
ddelacruz42 <91350250+ddelacruz42@users.noreply.github.com>
2023-07-24 07:00:54 -0700

    added route to editlog

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import HomePage from './pages/HomePage';
 import ResetPage from './pages/ResetPage';
 import AddLogPage from './pages/AddLogPage';
 import DiveCalculatorPage from './pages/DiveCalculatorPage';
+import EditLogPage from './pages/EditLogPage';
 
 function App() 
 {
@@ -24,6 +25,7 @@ function App()
       <Route path="/registerpage" index element={<RegisterPage />} />
       <Route path="/homepage" index element={<HomePage/>} />
       <Route path="/addlogpage" index element={<AddLogPage/>} />
+      <Route path="/editlogpage" index element={<EditLogPage />} />
       <Route path="/reset" index element={<ResetPage />} />
       <Route path="/divecalc" index element={<DiveCalculatorPage />} />
     </Routes>
