d5a6ef0cea4cefe2b551d4f4b0a8613a4bdf4828
ddelacruz42 <91350250+ddelacruz42@users.noreply.github.com>
2023-07-25 19:22:22 -0700

    updated placeholders for airpressure

--- frontend/src/components/AddLogUI.js
+++ frontend/src/components/AddLogUI.js
@@ -105,56 +105,6 @@ function AddLogUI()
                         </div>
                     </div>
 
-
-                    {/* <div className="diveDepths">
-                        <div className="data1">
-                            <label htmlfor="diveData1">
-                                diveData1
-                            </label>
-                            <input type="text" 
-                                className="addField" 
-                                id="divedata1" 
-                                placeholder="depth" 
-                                ref={(c) => diveData1 = c} 
-                            />
-                        </div>
-                        <div className="data2">
-                            <label htmlfor="diveData2">
-                                diveData2
-                            </label>
-                            <input type="text" 
-                                className="addField" 
-                                id="divedata2" 
-                                placeholder="depth" 
-                                ref={(c) => diveData2 = c} 
-                            />
-                        </div>
-                        <div className="data3">
-                            <label htmlfor="diveData3">
-                                diveData3
-                            </label>
-                            <input type="text" 
-                                className="addField" 
-                                id="divedata3" 
-                                placeholder="depth" 
-                                ref={(c) => diveData3 = c} 
-                            />
-                        </div>
-
-                        <div className="data4">
-                            <label htmlfor="diveData4">
-                                diveData4
-                            </label>
-                            <input type="text" 
-                                className="addField" 
-                                id="divedata4" 
-                                placeholder="depth" 
-                                ref={(c) => diveData4 = c} 
-                            />
-                        </div>
-                    </div> */}
-
-
                     <div className="locationAndDate">
                         <div className="locationDiv">
                             <label htmlFor="addLocation">
@@ -241,7 +191,7 @@ function AddLogUI()
                             <input type="text" 
                                 className="addField" 
                                 id="addLocation" 
-                                placeholder="Fahrenheit"
+                                placeholder="Bars"
                                 ref={(c) => startAirPressure = c}
                             />
                         </div>
@@ -252,7 +202,7 @@ function AddLogUI()
                             <input type="text" 
                                 className="addField" 
                                 id="addDate" 
-                                placeholder="Meters"
+                                placeholder="Bars"
                                 ref={(c) => endAirPressure = c}
                             />
                         </div>
